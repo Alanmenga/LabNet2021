42577c661034202b650d10aab0d9d15c9b4071c9
Alanmenga <alan.gabriel.mengassini@gmail.com>
2021-05-02 23:49:45 -0300

    Ejercicio5

---
+++ Mengassini.EF/Mengassini.EF.Logic/RegionLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mengassini.EF.Data;
+using Mengassini.EF.Entities;
+
+namespace Mengassini.EF.Logic
+{
+    public class RegionLogic : BaseLogic,IABMLogic<Region>
+    {
+        public List<Region> GetAll()
+        {
+            return context.Region.ToList();
+        }
+
+        public void Add(Region newRegion)
+        {
+            context.Region.Add(newRegion);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var regionAEliminar = context.Region.Find(id);
+            context.Region.Remove(regionAEliminar);
+            context.SaveChanges();
+        }
+
+        public void Update(Region region)
+        {
+            var regionUpdate = context.Region.Find(region.RegionID);
+            regionUpdate.RegionDescription = region.RegionDescription;
+            context.SaveChanges();
+        }
+    }
+}
