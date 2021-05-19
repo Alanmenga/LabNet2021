b5c1bd8ea04ac9c81ec43776dc74d4847c1ee8dc
Alanmenga <alan.gabriel.mengassini@gmail.com>
2021-05-19 00:08:28 -0300

    Algunos arreglos

---
+++ Mengassini.EF/Mengassini.WebApi/Models/ProductsView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mengassini.WebApi.Models
+{
+    public class ProductsView
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cantidad { get; set; }
+        public decimal? Precio { get; set; }
+        public short? UStock { get; set; }
+        public short? UOrdenadas { get; set; }
+    }
+}
